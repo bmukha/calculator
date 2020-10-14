373c8a3237a96cbe4477fa75a7569a294106c3de
bmukha <47007157+bmukha@users.noreply.github.com>
2020-10-15 00:51:21 +0300

    first step to calcilations

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 let displayValue = [];
+let state = "start";
+let firstOperand = "";
+let operation = "";
+let secondOperand = "";
 const calculator = document.querySelector("#calculator");
 const screen = document.querySelector("#screen");
 calculator.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn_digits")) {
-    // console.log(e.target.textContent);
-    updateDisplayValue(e.target.textContent);
-    screen.textContent = displayValue.join("");
+    if (state === "start" || state === "secondOperandStarted") {
+      updateDisplayValue(e.target.textContent);
+      screen.textContent = displayValue.join("");
+    }
+    if (state === "operationChosen") {
+      displayValue = [];
+      state = "secondOperandStarted";
+      updateDisplayValue(e.target.textContent);
+      screen.textContent = displayValue.join("");
+    }
+  }
+  if (e.target.classList.contains("btn_operation")) {
+    if (state === "start") {
+      firstOperand = screen.textContent;
+      switch (e.target.textContent) {
+        case "+":
+          operation = add;
+          break;
+        case "-":
+          operation = sub;
+          break;
+        case "*":
+          operation = mult;
+          break;
+        case "/":
+          operation = div;
+          break;
+      }
+      state = "operationChosen";
+    }
+    if (state === "secondOperandStarted") {
+      secondOperand = screen.textContent;
+      let result =
+        "" +
+        operate(
+          Number.parseFloat(firstOperand),
+          Number.parseFloat(secondOperand),
+          operation
+        );
+      displayValue = result.split("");
+      firstOperand = secondOperand;
+      secondOperand = "";
+      state = "start";
+    }
   }
 });
 
